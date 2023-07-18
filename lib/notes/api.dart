d2952b7a46201f220cadae91839e08d1693cd470
Mikeychocho <mikeychocho@gmail.com>
2023-07-18 22:14:36 +0100

    updated tutorial again

--- lib/notes/api.dart
+++ lib/notes/api.dart
@@ -8,3 +8,26 @@ void getData() async {
   // to change the data from string to object
   Map data = jsonDecode(response.body);
 }
+
+
+// request time url
+Future<void> getTime() async {
+  try {
+    // to get the data from api
+    http.Response response = await http.get(Uri.parse('https://worldtimeapi.org/api/timezone/'));
+    // to convert it to json from string
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3); // substring is slice() in js
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+  } catch (e) {
+    print('error occured: $e');
+  }
+}
